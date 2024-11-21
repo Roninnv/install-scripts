e0f3803f67f8d26dc2e7ba1315ab5730a0f830b9
Roninnv <AsA828A@outlook.com>
2024-11-21 18:34:27 +0800

    Thu Nov 21 06:34:27 PM CST 2024

--- golang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo -v
-
-wget https://dl.google.com/go/go1.23.3.linux-amd64.tar.gz
-tar -xzf go1.23.3.linux-amd64.tar.gz
-sudo mv go /usr/local/
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-source ~/.bashrc
